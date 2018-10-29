a58b2085a33a8d924210a510346aa89d0791a9bc
Samuel <webpagepro@aol.com>
2018-10-29 08:09:34 -0700

    part2

--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+let express = require('express');
+let app = express();
+let fs = require("fs");
+//let path = require("path");
+let port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
-});
+// Challenge 1
+app.post('/create/:name/:age', function (req, res) {
+  let person = {"name" : req.params.name,
+                 "age" : parseInt(req.params.age) 
+                 }
+        
+  let f = fs.readFileSync('./storage.json', 'utf8');
+  let content = JSON.parse(f);
+  content.push(person);
+  console.log(content);
+  fs.writeFileSync("/storage.json", JSON.stringify(content));
+  res.send('File write complete')
+})
+
+// Challenge 2
+app.get('/', function(req, res){
+  let h = fs.readFileSynce('./storage.json', 'utf8');
+  let content = JSON.parse(h);
+  let result = content.filter(item => item.name === req.params.name)[0];
+  if(result){
+    res.json(result);
+  }else{
+    res.sendStatus(400);
+  }
+})
+
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
